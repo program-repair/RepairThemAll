1446d8fc44d8a6f5f21b16ce98acaf12f5408f58
tdurieux <durieuxthomas@hotmail.com>
2018-11-10 22:26:43 +0100

    fix checkout

--- script/core/benchmarks/Bears.py
+++ script/core/benchmarks/Bears.py
@@ -70,7 +70,7 @@ cp -r . %s""" % (
 
     def compile(self, bug, working_directory):
         cmd = """cd %s;
-mvn install -V -B -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true; 
+mvn install -V -B -DskipTests -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true; 
 mvn test -DskipTests -V -B -Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true;
 mvn dependency:build-classpath -Dmdep.outputFile="classpath.info";
 """ % (working_directory)
