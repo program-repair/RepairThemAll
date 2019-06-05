b2d9c5151ec680647522fca20c079e0369c9aa29
tdurieux <durieuxthomas@hotmail.com>
2019-06-05 09:26:58 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ git submodule update;
 cd benchmarks/Bug-dot-jar/;
 git submodule init;
 git submodule update;
-cd ../../;
+cd ../defects4j;
+./init.sh
+cd ../../
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -DremoteRepositories=https://tdurieux.github.io/maven-repository/snapshots/ -Dartifact=com.github.tdurieux:project-config-maven-plugin:1.0-SNAPSHOT;
