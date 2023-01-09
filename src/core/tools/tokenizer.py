af8a141820809549b72f6b2cfd5d931745b951a4
Pengyu Zhao <pengyu@zhao.se>
2023-01-09 03:21:34 +0100

    Multi samples

--- src/core/tools/tokenizer.py
+++ src/core/tools/tokenizer.py
@@ -1,6 +1,8 @@
+from math import ceil, floor
 from transformers import GPT2TokenizerFast
 import os
 
+TOTAL_SAMPLES = 200
 MAX_TOKEN_LENGTH = 8000
 COMPLETION_RATIO = 1.2
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -17,3 +19,10 @@ def get_max_completion_size(prompt_size, bug_size):
     rest = max(int(MAX_TOKEN_LENGTH - prompt_size - bug_size), 0)
     completion_budget = bug_size * COMPLETION_RATIO
     return int(min(rest, completion_budget))
+
+
+def calculate_request_counter(prompt_size, bug_size):
+    completion_size = get_max_completion_size(prompt_size, bug_size)
+    n_value = max(floor((MAX_TOKEN_LENGTH - prompt_size) / completion_size), 1)
+    total_request = ceil(TOTAL_SAMPLES / n_value)
+    return total_request, n_value, completion_size
--- src/fixa.py
+++ src/fixa.py
@@ -1,11 +1,7 @@
-import os
 import argparse
 import time
-from core.database.engine import save
-from core.database.schema import Result
-from core.large_language_models.codex import apply_response_to_fixed_version, fix_bug_by_openai_codex, revert_response_to_fixed_version
+from core.large_language_models.codex import fix_single_bug
 
-from core.utils import get_benchmark
 
 parser = argparse.ArgumentParser(
     prog="fixa", description='Checkout and fix the bug by pre-trained large language model')
@@ -41,88 +37,7 @@ DEFECTS4J_BUG_SIZE = {
     'Time': 27,
 }
 
-
-def checkout_bug(benchmark, project, bug_id, version):
-    bug_identifier = args.project + '_' + bug_id
-
-    bug_path = os.path.join(args.working_directory,
-                            "%s_%s_%s_%s" % (benchmark.name, project, bug_id, version))
-
-    print('bug_identifier: ', bug_identifier)
-    bug = benchmark.get_bug(bug_identifier)
-    print('bug: ', bug)
-    is_buggy_version = version == 'buggy'
-    bug.checkout(bug_path, is_buggy_version)
-
-    return bug
-
-
-def fix_single_bug(args, bug_id, fixa_config):
-    result = Result()
-    result.buggy_code_chunk = ''
-    result.model = args.model
-    result.benchmark = args.benchmark
-    result.project = args.project
-    result.bug_id = bug_id
-    result.request_type = 'SINGLE_FUNCTION'
-
-    benchmark = get_benchmark(args.benchmark)
-
-    bug_dir = os.path.join(args.working_directory,
-                           "%s_%s_%s" % (args.benchmark, args.project, bug_id))
-
-    try:
-        fixed_bug = checkout_bug(benchmark, args.project, bug_id, 'fixed')
-    except Exception as e:
-        print('-------bug {} {} does not exist or deprecated-------\n'.format(args.project, bug_id), e)
-        return
-
-    if fixa_config['compile']:
-        fixed_bug.compile()
-    if fixa_config['test']:
-        fixed_bug.run_test()
-
-    buggy_bug = checkout_bug(benchmark, args.project, bug_id, 'buggy')
-    if fixa_config['compile']:
-        buggy_bug.compile()
-    if fixa_config['test']:
-        buggy_bug.run_test()
-
-    # Only support Codex with Defects4J for now
-    if args.model == 'Codex' and args.benchmark == 'Defects4J':
-        patch_file_path = 'benchmarks/defects4j/framework/projects/{}/patches/{}.src.patch'.format(
-            args.project, bug_id)
-        try:
-            applied, result, original_buy_lines = fix_bug_by_openai_codex(result, args.working_directory, fixed_bug, patch_file_path,
-                                                                          fixa_config['include_document'], fixa_config['include_comments'], fixa_config['dry_run'])
-            if applied:
-                print('bug from codex response has been applied')
-                # compile the fixed version with the response from Codex
-                compiled_output = fixed_bug.compile()
-                result.respond_compiled_output = compiled_output
-                if compiled_output.count('OK') == 2:
-                    result.result_type = 'COMPILED_SUCCESS'
-                test_output = fixed_bug.run_test()
-                if test_output == True:
-                    result.result_type = 'TEST_SUCCESS'
-                else:
-                    result.result_type = 'TEST_FAILED'
-                # revert the codex response version to the original fixed version
-                revert_response_to_fixed_version(
-                    original_buy_lines, args.working_directory, fixed_bug, patch_file_path)
-            save(result)
-        except Exception as e:
-            result.result_type = 'ERROR'
-            result.error_message = str(e)
-            save(result)
-            print(
-                '-------something wrong with bug {} {}-------'.format(args.project, bug_id), e)
-    else:
-        print('Only support Codex with Defects4J for now')
-        exit(1)
-
-
-fixa_config = {
+FIXA_CONFIG = {
     'include_document': False,
     'include_comments': True,
     'compile': False,
@@ -139,20 +54,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     dry_run = args.type == 'dryrun'
 
-    for round in range(fixa_config['sample']):
+    for round in range(FIXA_CONFIG['sample']):
         print('round: ', round + 1)
         if args.project != None and args.id == None:
             # fix all bugs from a project
             bug_size = DEFECTS4J_BUG_SIZE[args.project]
             for bug_id in range(1, bug_size + 1):
-                fix_single_bug(args, str(bug_id), fixa_config)
-                time.sleep(30)
+                fix_single_bug(args, str(bug_id), FIXA_CONFIG)
+                time.sleep(12)
         elif args.project == None and args.id == None:
             # fix all bugs from all projects
             for project, bug_size in DEFECTS4J_BUG_SIZE.items():
                 args.project = project
                 for bug_id in range(1, bug_size + 1):
-                    fix_single_bug(args, str(bug_id), fixa_config)
-                    time.sleep(30)
+                    fix_single_bug(args, str(bug_id), FIXA_CONFIG)
+                    time.sleep(12)
         else:
-            fix_single_bug(args, args.id, fixa_config)
+            fix_single_bug(args, args.id, FIXA_CONFIG)
